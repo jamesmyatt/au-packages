00232d9f17dbe58d05d511d6eedf496e31ac5c50
James Myatt <james.myatt@tessella.com>
2021-03-26 11:17:27 +0000

    Add miniforge3 package

---
+++ miniforge3/tools/chocolateyInstall.ps1
@@ -0,0 +1,67 @@
+﻿$ErrorActionPreference = 'Stop'
+$ToolsDir = Get-ToolsLocation
+$pp = Get-PackageParameters
+
+if (!$pp['InstallationType']) {
+    $InstallationType = 'AllUsers'
+}
+else {
+    if ($pp['InstallationType'] -notin 'AllUsers', 'JustMe') {
+        Write-Error "Value for InstallationType not recognised: only `'AllUsers`' or `'JustMe`' are valid"
+    }
+    else {
+        $InstallationType = $pp['InstallationType']
+    }
+}
+
+if (!$pp['RegisterPython']) {
+    $RegisterPython = '1'
+}
+else {
+    if ($pp['RegisterPython'] -notin '0', '1') {
+        Write-Error "Value for RegisterPython not recognised: only `'0`' or `'1`' are valid"
+    }
+    else {
+        $RegisterPython = $pp['RegisterPython']
+    }
+}
+
+if (!$pp['AddToPath']) {
+    $AddToPath = '0'
+}
+else {
+    if ($pp['AddToPath'] -notin '0', '1') {
+        Write-Error "Value for AddToPath not recognised: only `'0`' or `'1`' are valid"
+    }
+    else {
+        $AddToPath = $pp['AddToPath']
+    }
+}
+
+if (!$pp['D']) {
+    $D = Join-Path $ToolsDir 'miniforge3'
+}
+else {
+    if (!(Test-Path -IsValid $pp['D'])) {
+        Write-Error "Value for D ($($pp['D'])) is not a valid directory path"
+    }
+    else {
+        $D = $pp['D']
+    }
+}
+
+$packageArgs = @{
+    packageName            = 'miniforge3'
+    fileType               = 'exe'
+    url                    = 'https://github.com/conda-forge/miniforge/releases/download/4.9.2-7/Miniforge3-4.9.2-7-Windows-x86_64.exe'
+    url64bit               = 'https://github.com/conda-forge/miniforge/releases/download/4.9.2-7/Miniforge3-4.9.2-7-Windows-x86_64.exe'
+    checksum               = 'c2c80a13087ee70f14fa7abdf45b4808567c6236062c63b1a9476b4fb911b778'
+    checksum64             = 'c2c80a13087ee70f14fa7abdf45b4808567c6236062c63b1a9476b4fb911b778'
+    checksumType           = 'sha256'
+    checksumType64         = 'sha256'
+    silentArgs             = "/S /InstallationType=$InstallationType /RegisterPython=$RegisterPython /AddToPath=$AddToPath /D=$D"
+    validExitCodes         = @(0)
+    softwareName           = 'miniforge3*'
+  }
+
+Install-ChocolateyPackage @packageArgs
---
+++ miniforge3/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+$releases = 'https://github.com/conda-forge/miniforge/releases'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $url64   = $download_page.links | ? href -match 'Miniforge3-.*-Windows-x86_64\.exe$' | % href | select -First 1
+    $url32   = $url64
+    $version = (Split-Path ( Split-Path $url64 ) -Leaf) -replace '-', '.'
+
+    @{
+        URL32   = 'https://github.com' + $url32
+        URL64   = 'https://github.com' + $url64
+        Version = $version
+    }
+}
+
+update
